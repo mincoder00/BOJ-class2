7c6bfd541551db4af7afd48d2e08e3dc4e0922c9
mincoder00 <minchul0123@gmail.com>
2024-03-02 15:13:20 +0900

    2869

---
+++ 2869.cpp
@@ -0,0 +1,13 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	double a, b, v;
+	cin >> a >> b >> v;
+	cout<<fixed;
+	cout.precision(0);
+
+	//반복문으로 구현 시 시간 초과이므로 수식으로 정리한 부등식 이용
+	int x = ceil((v - a) / (a - b));
+	cout << x+1;
+}
