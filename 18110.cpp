fe1f833ad9113630ba58636b2611065f36d644af
mincoder00 <minchul0123@gmail.com>
2024-03-11 16:56:23 +0900

    Update SOLUTION

---
+++ 18110.cpp
@@ -0,0 +1,26 @@
+// round(n*0.15)
+// 오름차순 정렬 후 round만큼 앞뒤 빼고 sum+=
+// 평균
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int n;
+	float sum = 0;
+	cin >> n;
+	if (n == 0) {
+		cout << 0;
+		return 0;
+	}
+	int cut = round(n*0.15);
+	float* arr = new float[n];
+
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr, arr + n);
+	for (int i = 0+cut; i < n-cut; i++) sum +=arr[i];
+	cout << round(sum / (n - 2 * cut));
+	delete[] arr;
+	return 0;
+
+}
